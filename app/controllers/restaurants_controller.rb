688fb9852776f05376f650bc9d4ec8ab9558222f
Samir Zarouri <Zarourisamir@hotmail.com>
2021-06-16 16:21:47 +0200

    add routes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,7 @@ class RestaurantsController < ApplicationController
     def index 
         @restaurant = Restaurant.all
     end
+    def show
+    @restaurant = Restaurant.find(params[:id])
+    end
 end
