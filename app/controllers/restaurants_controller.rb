ae80d9ebe2263f7b9c753f90118834fbed012bf5
Samir Zarouri <Zarourisamir@hotmail.com>
2021-06-16 20:59:08 +0200

    review

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,6 +5,16 @@ class RestaurantsController < ApplicationController
     def show
     @restaurant = Restaurant.find(params[:id])
     end
+
+    def new
+        @restaurant = Restaurant.new
+    end
+    def create
+        @restaurant = Restaurant.new(restaurant_params)
+        @restaurant.save
+        redirect_to restaurant_path(@restaurant)
+    end
+
 end
 
 
--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -4,5 +4,18 @@ class ReviewController < ApplicationController
         @restaurant = Restaurant.find(params[:restaurant_id])
         @review = Review.new
       end
-    end
+      def create
+        @review = Review.new(review_params)
+        @restaurant = Restaurant.find(params[:restaurant_id])
+        @review.restaurant = @restaurant
+        if @review.save
+          redirect_to restaurant_path(@restaurant)
+        else
+          render :new
+        end
+      end
+
+private
+def review_params
+    params.require(:review).permit(:content)
 end
